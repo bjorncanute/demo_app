1b5b2a8a83ac6e77a57d113293ed033e0936ae4a
Bjorn <Bjorn@Bjorns-Mac-mini.local>
2014-02-18 18:38:39 -0600

    fixed weird messup

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219002906) do
+ActiveRecord::Schema.define(version: 20140219003807) do
 
   create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
